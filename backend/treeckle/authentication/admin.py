13bad95750250a5463d568eb7b96a9a225d24106
Jeremy Tan <tan_kai_qun97@yahoo.com.sg>
2021-07-10 03:29:24 +0800

    Add password auth model to admin site

--- backend/treeckle/authentication/admin.py
+++ backend/treeckle/authentication/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import GmailAuthentication, OpenIdAuthentication
+from .models import GmailAuthentication, OpenIdAuthentication, PasswordAuthentication
 
 # Register your models here.
 admin.site.register(GmailAuthentication)
 admin.site.register(OpenIdAuthentication)
+admin.site.register(PasswordAuthentication)
