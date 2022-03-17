12e4fe845437c28c7bceeae2247a3a80a325e685
Jeremy Tan <tan_kai_qun97@yahoo.com.sg>
2022-03-17 10:33:43 +0800

    Update dependencies

--- backend/treeckle/treeckle/common/parsers.py
+++ backend/treeckle/treeckle/common/parsers.py
@@ -1,11 +1,16 @@
+from typing import Optional
 from datetime import datetime
 
 from django.utils.timezone import make_aware
 
 
-def parse_datetime_to_ms_timestamp(date: datetime) -> int:
-    return round(date.timestamp() * 1000)
+def parse_datetime_to_ms_timestamp(date_time: Optional[datetime]) -> Optional[int]:
+    return round(date_time.timestamp() * 1000) if date_time is not None else None
 
 
-def parse_ms_timestamp_to_datetime(ms_timestamp: int) -> datetime:
-    return make_aware(datetime.fromtimestamp(ms_timestamp / 1000))
+def parse_ms_timestamp_to_datetime(ms_timestamp: Optional[int]) -> Optional[datetime]:
+    return (
+        make_aware(datetime.fromtimestamp(ms_timestamp / 1000))
+        if ms_timestamp is not None
+        else None
+    )
