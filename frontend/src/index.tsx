f94592114ef40b3ee4180828c9a3f9e1c9cc8332
Jonathan Loh <jonathanloh02@gmail.com>
2024-02-13 10:59:44 +0800

    Add Google Analytics

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,10 +3,12 @@ import "./index.scss";
 
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
+import ReactGA from "react-ga";
 
 import App from "./app";
 import reportWebVitals from "./reportWebVitals";
 
+ReactGA.initialize(process.env.GA_TRACKING_ID ?? "");
 ReactDOM.render(
   <StrictMode>
     <App />
@@ -17,4 +19,6 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(() =>
+  ReactGA.pageview(window.location.pathname + window.location.search),
+);
