7abd076f9d603d4f6d9f44ddc252dbe5cfad8918
Jonathan Loh <jonathanloh02@gmail.com>
2024-02-20 14:32:03 +0800

    Add search for bookings, users and user invites

--- backend/treeckle/users/admin.py
+++ backend/treeckle/users/admin.py
@@ -3,5 +3,12 @@ from django.contrib import admin
 from .models import User, UserInvite
 
 # Register your models here.
-admin.site.register(User)
-admin.site.register(UserInvite)
+class UserAdmin(admin.ModelAdmin):
+    search_fields = ['name__icontains', 'email__icontains', 'organization__name__icontains', 'role__icontains']
+    
+admin.site.register(User, UserAdmin)
+
+class UserInviteAdmin(admin.ModelAdmin):
+    search_fields = ['email__icontains', 'organization__name__icontains', 'role__icontains']
+
+admin.site.register(UserInvite,UserInviteAdmin)
